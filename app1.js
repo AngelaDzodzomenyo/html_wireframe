86d802dec48a05aa53f5ee831e882495f2f207e7
Angela Dzodzomenyo <enyodzo@gmail.com>
2021-07-29 23:19:41 -0500

    updtate js code

--- app1.js
+++ app1.js
@@ -3,10 +3,27 @@ console.log('Hello World!')
 let username = prompt('What is your Name?')
 console.log (username)
 
-document.write('<h2>Welcome ' + username + ',</h2>')
-
-if(username.toLowerCase() == 'angela'){
-    document.write('<h2>Welcome ' + username + 'we share a name!</h2>')
-} else {
-    document.write('<h2>Welcome' + 'Friend ' + '</h2>')    
+function getusername(){
+    if(username.toLowerCase() == 'angela'){
+        document.write('<h2>Welcome ' + username + ', we share a name!</h2>')
+    } else {
+        document.write('<h2>Welcome ' + username + ',</h2>')
+        document.write('<h3>Hi' + ' Friend! ' + '</h3>')
+        
+    }
 }
+
+getusername();
+
+let askusercolor = prompt(username + '! Do you like the color blue? Yes or No.')
+console.log (askusercolor)
+
+function getusercolor(){
+    if(askusercolor.toLowerCase() == 'yes'){
+        alert("AWESOME!")   
+    } else {
+        alert("ew!")
+    }
+}
+    
+getusercolor();
