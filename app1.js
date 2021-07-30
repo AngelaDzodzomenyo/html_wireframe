ebe50cbecdf7e06683ff10f75351424b74e6efcb
Angela Dzodzomenyo <enyodzo@gmail.com>
2021-07-30 13:42:05 -0500

    update code

--- app1.js
+++ app1.js
@@ -22,15 +22,17 @@ function getusercolor(){
     if(askusercolor.toLowerCase() == 'yes'){
         alert("AWESOME!")   
     } else {
-        alert("ew!")
+        alert("That's ok!")
     }
 }
     
 getusercolor();
 
 function picturesonpage(){
-    let numberofpictures = prompt('Wanna see some cool cars?!')
-    for(let i = 0); i < numberofpictures; i++){
-        document.write('<img src="https://images.unsplash.com/photo-1550615162-30dcc978d172?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80, https://images.unsplash.com/photo-1507722661946-ec4abdfe6a1e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1301&q=80 ">')
+    let numberofpictures = prompt('How many cool cars do you want to see?')
+    for(let i = 0; i < numberofpictures; i++){
+        document.write('<img src="https://images.unsplash.com/photo-1550615162-30dcc978d172?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80">')
     }
 }
+
+picturesonpage();   
