8d84d33e7fa7466ca41038bca69d62e872e4f6cf
Angela Dzodzomenyo <enyodzo@gmail.com>
2021-08-29 08:50:47 -0500

    test

--- app1.js
+++ app1.js
@@ -1,38 +1,87 @@
 console.log('Hello World!')
 
-let username = prompt('What is your Name?')
-console.log (username)
+function unixEpochTime(){
+  var d = new Date()
+  return d.getTime();
+}
 
-function getusername(){
-    if(username.toLowerCase() == 'angela'){
-        document.write('<h2>Welcome ' + username + ', we share a name!</h2>')
-    } else {
-        document.write('<h2>Welcome ' + username + ',</h2>')
-        document.write('<h3>Hi' + ' Friend! ' + '</h3>')
+var testAlpha = unixEpochTime();
+var testBeta = unixEpochTime;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let username = prompt('What is your Name?')
+// console.log (username)
+
+// function getusername(){
+//     if(username.toLowerCase() == 'angela'){
+//         document.write('<h2>Welcome ' + username + ', we share a name!</h2>')
+//     } else {
+//         document.write('<h2>Welcome ' + username + ',</h2>')
+//         document.write('<h3>Hi' + ' Friend! ' + '</h3>')
         
-    }
-}
+//     }
+// }
 
-getusername();
+// getusername();
 
-let askusercolor = prompt(username + '! Do you like the color blue? Yes or No.')
-console.log (askusercolor)
+// let askusercolor = prompt(username + '! Do you like the color blue? Yes or No.')
+// console.log (askusercolor)
 
-function getusercolor(){
-    if(askusercolor.toLowerCase() == 'yes'){
-        alert("AWESOME!")   
-    } else {
-        alert("That's ok!")
-    }
-}
+// function getusercolor(){
+//     if(askusercolor.toLowerCase() == 'yes'){
+//         alert("AWESOME!")   
+//     } else {
+//         alert("That's ok!")
+//     }
+// }
     
-getusercolor();
+// getusercolor();
 
-function picturesonpage(){
-    let numberofpictures = prompt('How many cool cars do you want to see?')
-    for(let i = 0; i < numberofpictures; i++){
-        document.write('<img src="https://images.unsplash.com/photo-1550615162-30dcc978d172?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80">')
-    }
-}
+// function picturesonpage(){
+//     let numberofpictures = prompt('How many cool cars do you want to see?')
+//     for(let i = 0; i < numberofpictures; i++){
+//         document.write('<img src="https://images.unsplash.com/photo-1550615162-30dcc978d172?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80">')
+//     }
+// }
 
-picturesonpage();   
+// picturesonpage();   
